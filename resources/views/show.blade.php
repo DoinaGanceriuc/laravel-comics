85dd57939771d18174d63511678952c30824474e
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-12-20 16:19:42 +0100

    add image single album

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,15 +4,28 @@
 
 @section('content')
 
-   <div class="container">
-        <div class="thumbs row">
-            <div class="thumb col-2">
-        <img src="{{ $album['thumb'] }}" alt="{{ $album['title'] }}">
-         <h5> {{ strtoupper($album['series']) }} </h5>
+<div class="bgblue">
+    <div class="container">
+         <div class="thumb">
+            <div class="album">
+                <img src="{{ $album['thumb'] }}" alt="{{ $album['title'] }}">
+                <p class="type_album">{{ $album['type'] }}</p>
+                <small class="view_gallery">
+                    <a href="#">view gallery</a>
+                </small>
+            </div>
+            <!-- /.album -->
         </div>
+           <!-- /.thumb -->
     </div>
-    <!-- /.thumbs -->
-   </div>
+    <!-- /.container -->
+</div>
+<!-- /.bgblue -->
+
+<div class="container">
+
+</div>
+<!-- /.container -->
 
 
 
