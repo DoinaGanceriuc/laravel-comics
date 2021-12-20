5aed109d230c72f3489d57a8032dfdc2ccd884db
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-12-20 17:54:00 +0100

    complete section description single album

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -22,10 +22,37 @@
 </div>
 <!-- /.bgblue -->
 
-<div class="container">
-
+<div class="single_album container">
+   <div class="row d_flex">
+       <div class="col-75">
+            <h2> {{ strtoupper($album['title']) }} </h2>
+            <div class="single_album_info">
+                <div class="avaiable col-75">
+                    <p>U.S. Price: <span>{{ $album['price'] }}</span></p>
+                    <p>AVAIABLE</p>
+                </div>
+                <!-- /.avaiable -->
+                <div class="check col-25 t_center">
+                    <p>Check Availability</p>
+                </div>
+                 <!-- /.check -->
+            </div>
+            <!-- /.single_album_info -->
+            <div class="description">
+                <p>{{ $album['description'] }} </p>
+            </div>
+            <!-- /.description -->
+        </div>
+        <!-- /.col-75 -->
+        <div class="col-25 rewards">
+            <p>ADVERTISEMENT</p>
+            <img src="{{asset('img/unleash.jpg')}}" alt="Superman">
+        </div>
+        <!-- /.col-25 -->
+    </div>
+    <!-- /.row -->
 </div>
-<!-- /.container -->
+<!-- /.single_album container -->
 
 
 
