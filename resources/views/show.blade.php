15e859bfa4a32caab98d48083907fbe8be1be4ea
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-12-20 18:56:07 +0100

    add specifications artists and writers

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -53,6 +53,86 @@
     <!-- /.row -->
 </div>
 <!-- /.single_album container -->
+<div class="specifications">
+    <div class="container">
+        <div class="row">
+            <div class="col-50 talent">
+            <h3>Talent</h3>
+            <div class="artists">
+                <div class="col-25">
+                    <p>Art by:</p>
+                </div>
+                <!-- /.col-25 -->
+                <div class="col-75">
+                    <p> @foreach($album['artists'] as $artist)
+                    {{$artist}},
+                    @endforeach
+                    </p>
+                </div>
+                <!-- /.col-75 -->
+            </div>
+            <!-- /.artists -->
+            <div class="writters">
+                <div class="col-25">
+                    <p>Written by:</p>
+                </div>
+                <!-- /.col-25 -->
+               <div class="col-75">
+                    <p> @foreach($album['writers'] as $writer)
+                    {{$writer}},
+                    @endforeach
+
+                    </p>
+               </div>
+               <!-- /.col-75 -->
+            </div>
+            <!-- /.writers -->
+        </div>
+        <!-- /.col-50 -->
+        <div class="col-50 specs">
+            <h3>Specs</h3>
+            <div class="info_specs">
+                <div class="col-25">
+                    <p>Series:</p>
+                </div>
+                <!-- /.col-25 -->
+                <div class="col-75">
+                    <p class="series">{{ strtoupper($album['series']) }}</p>
+                </div>
+                <!-- /.col-75 -->
+            </div>
+            <!-- /.info_specs -->
+             <div class="info_specs">
+                <div class="col-25">
+                    <p>U.S. Price:</p>
+                </div>
+                <!-- /.col-25 -->
+                <div class="col-75">
+                    <p>{{ $album['price'] }}</p>
+                </div>
+                <!-- /.col-75 -->
+            </div>
+            <!-- /.info_specs -->
+            <div class="info_specs date">
+                <div class="col-25">
+                    <p>On Sale Date:</p>
+                </div>
+                <!-- /.col-25 -->
+                <div class="col-75">
+                    <p>{{ $album['sale_date'] }}</p>
+                </div>
+                <!-- /.col-75 -->
+            </div>
+            <!-- /.info_specs -->
+        </div>
+        <!-- /.col-50 -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+
+</div>
+<!-- /.specifications -->
 
 
 
